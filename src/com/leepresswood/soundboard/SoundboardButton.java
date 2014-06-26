1e16c401e70cd7359f01d1f34350bcc9e39e9540
Lee Presswood <pressedlee123@gmail.com>
2014-06-25 21:18:27 -0500

    Starting to work on the right click of buttons

--- src/com/leepresswood/soundboard/SoundboardButton.java
+++ src/com/leepresswood/soundboard/SoundboardButton.java
@@ -1,13 +1,27 @@
 package com.leepresswood.soundboard;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import javax.swing.Action;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class SoundboardButton extends JButton
 {
 	public int button_number;
+	public File new_file;
 	
 	public SoundboardButton(int button_number, String text)
 	{
@@ -42,8 +56,33 @@ public class SoundboardButton extends JButton
 	}
 	
 	private void onRightClick()
-	{
-		JOptionPane.showInputDialog("Test");
+	{		
+		//File chooser
+		final JTextArea field = new JTextArea(5,20);
+		JButton file_button = new JButton("...");
+		final JFileChooser fc = new JFileChooser();		
+		//fc.showDialog(sb, "Hi");
+		file_button.addActionListener(new ActionListener()
+		{			
+			@Override
+			public 
+			void actionPerformed(ActionEvent arg0)
+			{
+				fc.showOpenDialog(SoundboardButton.this);
+				field.setText(fc.getSelectedFile().toPath().toString());
+				new_file = fc.getSelectedFile();
+			}
+		});
+		
+		JDialog dialog = new JDialog();
+		dialog.setSize(new Dimension(500, 350));
+		dialog.setPreferredSize(dialog.getSize());
+		dialog.setMinimumSize(dialog.getSize());
+		dialog.setContentPane(new JPanel());
+		dialog.add(new JLabel("Sound File:"));
+		dialog.add(field);
+		dialog.add(new JButton("..."));
+		dialog.setVisible(true);
 	}
 	
 	private void onLeftClick()
